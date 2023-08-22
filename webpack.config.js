66c2a2d96edf225c46dd19d9ce58d59cd0f21c24
Dialejo24 <dialejosi530@gmail.com>
2023-08-21 20:47:27 -0500

    Change webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,22 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  }
 }
