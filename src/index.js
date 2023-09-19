732523a26076148d7ad89280784f95835afdb6a6
Dialejo24 <dialejosi530@gmail.com>
2023-09-18 22:18:47 -0500

    Complete successful-card version desktop

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+
+let successfulCard = document.querySelector(".successful-card");
+let mainContainer = document.querySelector(".container");
+let subscribeButton = document.querySelector(".subscribe");
+let dismissButton = document.querySelector(".dismiss");
+
+subscribeButton.addEventListener("click", e => {
+    mainContainer.classList.add("hide-content");
+    successfulCard.classList.remove("hide-content");
+})
+
+dismissButton.addEventListener("click", e => {
+    mainContainer.classList.remove("hide-content");
+    successfulCard.classList.add("hide-content");
+})
